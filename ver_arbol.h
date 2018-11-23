7fef7042fc4a1e6cfae80c04f5e0c0f574a91f09
masacr3 <leorhcp2030@gmail.com>
2018-11-23 00:48:30 -0300

    remove verArbol

--- ver_arbol.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include "strutil.h"
-#include "abb.h"
-#include "funciones_auxiliares.h"
-
-bool ver_arbol(abb_t* abb);
