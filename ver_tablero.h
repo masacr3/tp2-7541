a984fa6b7cc6534dedc4d25bbdf6a871e4064de5
masacr3 <leorhcp2030@gmail.com>
2018-11-23 02:43:18 -0300

    remove ver_tablero .c .h

--- ver_tablero.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include "strutil.h"
-#include "abb.h"
-#include "funciones_auxiliares.h"
-
-bool ver_tablero(char** comandos,abb_t* abb);
