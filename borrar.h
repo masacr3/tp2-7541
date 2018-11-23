35a3e19e9877777f086dfbcc92dda3fcc91b1384
masacr3 <leorhcp2030@gmail.com>
2018-11-23 02:38:59 -0300

    remove borrar .c .h

--- borrar.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include "strutil.h"
-#include "abb.h"
-#include "funciones_auxiliares.h"
-
-bool borrar(char **comandos,abb_t* abb,hash_t* hash);
