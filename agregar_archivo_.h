d2baf8dc4ef9c30add89a2577a61c49680f2e2b3
masacr3 <leorhcp2030@gmail.com>
2018-11-22 22:26:06 -0300

    removi agregarArchivo .c .h

--- agregar_archivo_.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "hash.h"
-#include "lista.h"
-#include "abb.h"
-#include <stdbool.h>
-#include <stdlib.h>
-
-bool agregar_archivo(char **comandos, hash_t* hash,abb_t* abb);
